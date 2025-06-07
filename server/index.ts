afeb9af06bd9505983b21b6ccb9d360fbd1de2c0
rotimi nicol <rotiminicol117@gmail.com>
2025-06-07 03:19:25 +0100

    update

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 8080; // Changed to a different port
-  const host = '127.0.0.1';
+  const port = process.env.PORT || 8080;
+  const host = '0.0.0.0';
 
   server.on('error', (error: any) => {
     if (error.syscall !== 'listen') {
